a1e437d27d3173593f14fd4a90029836b208a14e
tad0616 <tad0616@gmail.com>
2019-05-10 22:32:12 +0800

    修改說明

--- include/onUpdate.php
+++ include/onUpdate.php
@@ -1,5 +1,9 @@
 <?php
 use XoopsModules\Tadtools\Utility;
+if (!class_exists('XoopsModules\Tadtools\Utility')) {
+    require XOOPS_ROOT_PATH . '/modules/tadtools/preloads/autoloader.php';
+}
+
 function xoops_module_update_tad_tv($module, $old_version)
 {
     global $xoopsDB;
